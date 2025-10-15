d056b9e63d2f9498bf8470064ee05e345a3ef56e
Spiros Papadimitriou <spapadim@gmail.com>
2025-10-15 14:46:24 -0400

    Fix virtualenv prompt segment in devcontainer
    VSCode seems to have changed how it activates venv (again?), so
    it's no longer sufficient to check if VIRTUAL_ENV is set only at
    startup and define prompt_virtualenv() function accordingly. Thus,
    moved check into function itself.

--- .devcontainer/agnoster-custom.zsh
+++ .devcontainer/agnoster-custom.zsh
@@ -1,7 +1,9 @@
-if [[ -z "$VIRTUAL_ENV" ]]; then
-  # Show warning message (should hopefully reduce dangerous confusion?)
-  prompt_virtualenv() { prompt_segment red white '\u26a0 \u27e8no python env\u27e9' }
-else
-  # Devcontainer sets VIRTUAL_ENV_DISABLE_PROMPT, so no need to check...
-  prompt_virtualenv() { prompt_segment blue black "(${VIRTUAL_ENV:t:gs/%/%%})" }
-fi
+# Devcontainer sets VIRTUAL_ENV_DISABLE_PROMPT, so no need to check...
+# Also, if no virtual env, show warning message (should hopefully reduce dangerous confusion?)
+prompt_virtualenv() { 
+  if [[ -z "$VIRTUAL_ENV" ]]; then
+    prompt_segment red white '\u26a0 \u27e8no python env\u27e9' 
+  else
+    prompt_segment blue black "(${VIRTUAL_ENV:t:gs/%/%%})"
+  fi
+}
